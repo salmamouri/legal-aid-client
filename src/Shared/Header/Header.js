3f3dff449f6a334850132e04daadb1298577567b
salmamouri <mouriclub@gmail.com>
2022-11-08 19:25:09 +0600

    router setup

---
+++ src/Shared/Header/Header.js
@@ -0,0 +1,12 @@
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <Heading>Header</Heading>
+    </div>
+  );
+};
+
+export default Header;
